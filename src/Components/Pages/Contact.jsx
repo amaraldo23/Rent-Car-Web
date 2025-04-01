52b2b7df537eea0058ffd501172476097cba7d2d
amaraldo23 <amaraldoo.zhurda@gmail.com>
2025-04-01 13:37:29 +0200

    update

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { useState } from 'react';
 import './Contact.css'; // Importo stilizimet e reja
 
 function Contact() {
     return (
-        <div className="conact-container">
-            <h4>amaraldo</h4>
-        </div>
+        <section className="contact-container">
+            <div>
+            <h1 className="contact-title">Online car rental</h1>
+            <h4 className='contact-info'>
+            Rentout's evolution as an integrated car sharing and carpooling platform is a testament to the power of innovation in addressing the challenges of contemporary urban mobility. By offering a holistic solution that combines efficiency, sustainability, and user convenience, Rentout stands as a driving force in shaping a future where transportation goes beyond individual ownership to become a shared, community-driven experience. As Rentout continues to pave the way for integrated mobility solutions, it leaves an indelible mark on the landscape of sustainable urban transportation.
+            </h4>
+            </div>
+            <div className="contact-card"> 
+                <h2>Book with no hidden fees</h2>
+                <p>We do not require or block credit card amounts as a deposit. Booking process takes only 2 minutes</p>
+            </div>
+        </section>
     );
 }
 
